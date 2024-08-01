6cb6233aae99d0e13cca37a599cccc800dd1644c
Darieth <darieth.s.v@gmail.com>
2024-08-01 10:45:56 -0500

    Cambios en la función home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Darieth Sánchez'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page')
