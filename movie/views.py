30cc8f08264172ea56e8a8e15b2823c2b9386769
Darieth <darieth.s.v@gmail.com>
2024-08-01 17:29:34 -0500

    Búsqueda de películas implementada

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name':'Darieth Sánchez'})
+    #return render(request, 'home.html', {'name':'Darieth Sánchez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html',{'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page')
