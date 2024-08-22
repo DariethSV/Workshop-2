3b9db7cc184fe04cb4b76979c1cdf90ef779be2c
Darieth <darieth.s.v@gmail.com>
2024-08-22 11:52:44 -0500

    News implementation

--- moviesreviews/settings.py
+++ moviesreviews/settings.py
@@ -19,10 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-17zn6l2i566*x=*+_*$#1cd6j=av&&(crflw4v3c!()ezto(u9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -41,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +56,7 @@ ROOT_URLCONF = 'moviesreviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviesreviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
